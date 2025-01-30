faf8ccd41506c2bb2bb69e263604658f47924af1
asiaHH <77129000+asiaHH@users.noreply.github.com>
2025-01-30 11:05:12 +0100

    cp checkin

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ cd res && cat * | tar -xvz && cd ..
 mkdir -pv ready
 python src/fix_csv.py res/yelp_academic_dataset_tip.csv ready/yelp_academic_dataset_tip.csv
 cat res/yelp_academic_dataset_business.json | awk -f src/fix_json.awk > ready/yelp_academic_dataset_business.json
+cp res/yelp_academic_dataset_checkin.json ready/
 
 export PATH="/usr/gide/sbt-1.3.13/bin/:$PATH"
 
